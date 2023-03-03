8f024d20cc91ecea1b801043bbfef55861b839b3
wolf-null <gelzestern@yandex.ru>
2023-03-03 14:19:02 +0300

    initial commit

---
+++ www/index.test.js
@@ -0,0 +1,7 @@
+"use strict"
+
+//let test_var = "I'm the testing var"
+
+document.write("<p> This is an output via <i>write()</i> </p>")
+document.getElementById('js-output-placeholder').innerHTML = "[ <b>This</b> is an output via <i>getElementById()</i> ]"
+
