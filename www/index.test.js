13389148234e66547f8e8bdffc91496dc43761b7
wolf-null <wolfnull@yandex.ru>
2023-03-22 14:42:05 +0300

    aligment and a little of nested cascading

--- www/index.test.js
+++ www/index.test.js
@@ -8,14 +8,19 @@ document.write("<p> This is an output via <i>write()</i> </p>");
 // This thing finds a placeholder where id closure used (e.g. <someTag id="placeholder-name"> ... </someTag>)
 document.getElementById('js-output-placeholder').innerHTML = `[ <b>This</b> is an output of <i class="code">${test_var}</i> via <i>getElementById()</i> ]`;
 
+function term_decorate(txt) {
+	var txt_decorated = "&gt;&nbsp;" + '<span class="line">'+ txt + '</span>';
+	return txt_decorated;
+}
+
 function term(txt, placeholder) {
 	var current_text = document.getElementById(placeholder).innerHTML
 	var new_text;
-
+	var txt_decorated = term_decorate(txt);
 	if (current_text == "") {
-		new_text = txt;
-	} else {
-		new_text = current_text + "<br>" + txt;
+		new_text = txt_decorated;
+	} else {		
+		new_text = current_text + "<br>" + txt_decorated;
 	}
 	document.getElementById(placeholder).innerHTML = new_text;
 }
